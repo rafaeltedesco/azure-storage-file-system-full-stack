45b94654211d1cb04c98a8b8ac306e276a1dc0d5
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-18 21:51:09 -0300

    refactor: create containerClient service

---
+++ backend/src/azure-blob/providers/containerClient.service.ts
@@ -0,0 +1,42 @@
+import { BlobServiceClient } from '@azure/storage-blob';
+import { Injectable } from '@nestjs/common';
+import { AzureAuthProvider } from 'src/authenticators/providers/auth.service';
+import {
+  ContainerClientDTO,
+  ServiceResponseOrError,
+} from 'src/shared/types/serviceResponse';
+
+import {
+  ServiceResponseErrorStatus,
+  ServiceResponseSuccessStatus,
+} from 'src/shared/enums/serviceResponseEnum';
+
+@Injectable()
+export class ContainerClientService {
+  private readonly azureClient: BlobServiceClient;
+
+  constructor(private azureAuthProvider: AzureAuthProvider) {
+    this.azureClient = this.azureAuthProvider.getClient();
+  }
+
+  public async getContainerByName(
+    containerName: string,
+  ): Promise<ServiceResponseOrError<ContainerClientDTO>> {
+    const containerClient = this.azureClient.getContainerClient(containerName);
+    const exists = await containerClient.exists();
+    if (!exists) {
+      return {
+        status: ServiceResponseErrorStatus.INVALID_DATA,
+        data: {
+          error: `Container ${containerName} does not exist`,
+        },
+      };
+    }
+    return {
+      status: ServiceResponseSuccessStatus.OK,
+      data: {
+        containerClient,
+      },
+    };
+  }
+}
