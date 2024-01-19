4df03defdfb9455b2a29d180bab67540e27677a5
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-18 21:53:19 -0300

    refactor: add new controllers and providers in module

--- backend/src/azure-blob/index.ts
+++ backend/src/azure-blob/index.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AzureBlobController } from './controllers/azureBlobController';
 import { AzureAuthModule } from 'src/authenticators';
+import { UploadFilesController } from './controllers/uploadFiles.controller';
+import { UploadFileService } from './providers/uploadFiles.service';
+import { ContainerClientService } from './providers/containerClient.service';
+import { ListFilesController } from './controllers/listFiles.controller';
+import { ListFilesService } from './providers/listFiles.service';
 
 @Module({
   imports: [AzureAuthModule],
-  controllers: [AzureBlobController],
+  controllers: [UploadFilesController, ListFilesController],
+  providers: [UploadFileService, ContainerClientService, ListFilesService],
 })
 export default class AzureBlobModule {}
