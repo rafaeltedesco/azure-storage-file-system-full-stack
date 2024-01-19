52477cc2ff889f154a340db7abc3ad2540089e09
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-19 20:20:23 -0300

    feat: add router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './pages/Home/index.tsx';
+import ListContainers from './pages/ListContainers/index.tsx';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element:  <Home />
+}, {
+  path: '/list-containers',
+  element: <ListContainers />
+}]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
