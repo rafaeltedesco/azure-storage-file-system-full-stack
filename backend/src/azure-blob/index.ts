b6726b43489129b9e8e48f831d04ee4d5282a436
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-19 17:04:01 -0300

    feat: add new features in module

--- backend/src/azure-blob/index.ts
+++ backend/src/azure-blob/index.ts
@@ -5,10 +5,25 @@ import { UploadFileService } from './providers/uploadFiles.service';
 import { ContainerClientService } from './providers/containerClient.service';
 import { ListFilesController } from './controllers/listFiles.controller';
 import { ListFilesService } from './providers/listFiles.service';
+import { CreateContainerController } from './controllers/createContainer.controller';
+import { CreateContainerService } from './providers/createContainer.service';
+import { ListContainersController } from './controllers/listContainers.controller';
+import { ListContainerService } from './providers/listContainers.service';
 
 @Module({
   imports: [AzureAuthModule],
-  controllers: [UploadFilesController, ListFilesController],
-  providers: [UploadFileService, ContainerClientService, ListFilesService],
+  controllers: [
+    UploadFilesController,
+    ListFilesController,
+    CreateContainerController,
+    ListContainersController,
+  ],
+  providers: [
+    UploadFileService,
+    ContainerClientService,
+    ListFilesService,
+    CreateContainerService,
+    ListContainerService,
+  ],
 })
 export default class AzureBlobModule {}
