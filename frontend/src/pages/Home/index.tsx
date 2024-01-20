47ab8a1889578b66f846ad338944ac4cbd1f9be1
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-19 21:54:44 -0300

    refactor: add new components like header

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,13 +3,15 @@ import { useDropzone } from 'react-dropzone'
 import SendButton from '../../components/Buttons/SendButton';
 import { useCallback, useMemo, useState } from 'react';
 import { objectUrl } from '../../types/Buttons';
-import ContainerName from '../../components/Inputs/ContainerName';
+import { ImageUploaderContext } from '../../providers/ImageUploaderContext';
+import ContainerNameInput from '../../components/Inputs/ContainerNameInput';
+import Header from '../../components/Header';
 
 export default function Home() {
 
   const [files, setFiles] = useState<[File, objectUrl][]>([]);
   const [containerName, setContainerName] = useState('');
-
+  
   const onDrop = useCallback((acceptedFiles: File[]) => {
     const newFiles = acceptedFiles;
     const newTempFileList: [File, objectUrl][] = [];
@@ -50,34 +52,33 @@ export default function Home() {
     )}), [files])
 
   return (
-    <main className="outer-container">
-      <header>
-        <h1>Azure Storage File System</h1>
-        <h3><a href="/list-containers">List Containers</a></h3>
-      </header>
-      <section className="container">
-         <aside className="side-bar">
-          <div className="side-bar-header">
-            <h4 className="title">{files.length? "Files" : "Upload New Images"}</h4>
-            <hr />
-          </div>
-          { !!files.length && (
-            <div className="side-bar-body">
-              <ul className="files-list">{renderFiles}</ul>
-              <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
-                <ContainerName handleContainerName={{ setContainerName, containerName }}/>
-                <SendButton resetFiles={()=> setFiles([])} files={files} containerName={containerName} />
-              </div>
+    <ImageUploaderContext.Provider value={{ containerName, setContainerName}}>
+      <main className="outer-container">
+        <Header />
+        <section className="container">
+          <aside className="side-bar">
+            <div className="side-bar-header">
+              <h4 className="title">{files.length? "Files" : "Upload New Images"}</h4>
+              <hr />
             </div>
-          )}
-        </aside>
-        <div className="image-container">
-          <div {...getRootProps({className: 'dropzone'})} className="dropzone">
-            <input {...getInputProps()} />
-            <p className="drag-message">Drag 'n' drop some image files here, or click to select files</p>
+            { !!files.length && (
+              <div className="side-bar-body">
+                <ul className="files-list">{renderFiles}</ul>
+                <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
+                  <ContainerNameInput />
+                  <SendButton resetFiles={()=> setFiles([])} files={files} />
+                </div>
+              </div>
+            )}
+          </aside>
+          <div className="image-container">
+            <div {...getRootProps({className: 'dropzone'})} className="dropzone">
+              <input {...getInputProps()} />
+              <p className="drag-message">Drag 'n' drop some image files here, or click to select files</p>
+            </div>
           </div>
-        </div>
-    </section>
-  </main>
+      </section>
+    </main>
+  </ImageUploaderContext.Provider>
   )
 }
